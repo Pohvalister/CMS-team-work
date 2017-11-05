bf1b1aa414c4530941d5bfdbc1d3d5180af43620
Vlad-kv <vladislav-a-k@mail.ru>
2017-11-05 23:41:42 +0300

    slow dynamic picture

--- Newtons_method/Newton's_method.pro
+++ Newtons_method/Newton's_method.pro
@@ -4,14 +4,19 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets printsupport
 TARGET = Newtons_method
 TEMPLATE = app
 
+QMAKE_CXXFLAGS += -std=c++11
+QMAKE_CXXFLAGS += -O2
+
 SOURCES += main.cpp\
         mainwindow.cpp \
          ../qcustomplot.cpp \
-    newtons_method.cpp
+    newtons_method.cpp \
+    q_tree.cpp
 
 HEADERS  += mainwindow.h \
          ../qcustomplot.h \
-    newtons_method.h
+    newtons_method.h \
+    q_tree.h
 
 FORMS    += mainwindow.ui
 
---
+++ Newtons_method/q_tree.h
@@ -0,0 +1,45 @@
+#ifndef Q_TREE_H
+#define Q_TREE_H
+
+#include "mainwindow.h"
+#include <iostream>
+using namespace std;
+
+class rectangle {
+public:
+    rectangle(double x_1, double y_1, double x_2, double y_2);
+    rectangle& operator=(const rectangle& another);
+    
+    double square() const;
+    rectangle intersect(const rectangle& another) const;
+    
+    double x_1, y_1, x_2, y_2;
+};
+
+class q_tree {
+// Some point (x, y) belongs to q_tree only if (x_1 <= x < x_2) and (y_1 <= y < y_2)
+public:
+    static const int COLOR_MAP_SIZE_1 = 100;
+    static const int COLOR_MAP_SIZE_2 = 100;
+    static const int OPTIMAL_DEPTH = 1;
+    
+    q_tree(double x_1, double y_1, double x_2, double y_2);
+    
+    void initColorMap(QCustomPlot *plot);
+    void deleteColorMap(QCustomPlot *plot);
+    
+    void update(int required_depth, const rectangle& range, QCustomPlot *plot);
+    
+    static void init(QCustomPlot *plot, double x_1, double y_1, double x_2, double y_2);
+    
+    static void update_tree(QCustomPlot *plot);
+private:
+    q_tree *field[2][2];
+    int min_depth = 0;
+    QCPColorMap *colorMap = nullptr;
+    rectangle rect;
+};
+
+extern q_tree *root;
+
+#endif // Q_TREE_H
