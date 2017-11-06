013a99260f74792b98156ba586e95464bdc20779
kant97 <kirant9797@gmail.com>
2017-11-06 08:34:25 +0300

    new functions for good point generation

--- simple_iterations/main.cpp
+++ simple_iterations/main.cpp
@@ -6,15 +6,16 @@
 
 int main(int argc, char *argv[])
 {
-   const double r = 2.0;
+   const double r = 1.5;
 
    QApplication a(argc, argv);
    MainWindow w(nullptr, r);
    w.show();
 
-   std::vector<double> points = get_seq_iteration_points(r);
-   for (auto p: points) {
-      w.addStraightLine(p);
+   bool f = true;
+   for (size_t i = 0; i < get_amount_of_iterations(r); ++i) {
+      w.addStraightLine(get_next_point(r, f));
+      f = false;
    }
 
    return a.exec();
